cdf08940ed3c14823d45651a499445946bc1ddb7
BerdiaVardiashvili <berdiavardiashvili@gmail.com>
2023-04-28 13:08:25 +0300

    Update main.js

--- ModReal/Dz2/main.js
+++ ModReal/Dz2/main.js
@@ -14,18 +14,24 @@ const material1 = new THREE.MeshBasicMaterial({
 color: 0x00ffff, transparent: true, opacity: 0.5
 });
 const plane = new THREE.Mesh(geometry1, material1);
+plane.position.x = 1;
+plane.position.y = -0.75;
 
 const geometry2 = new THREE.BoxGeometry( 1, 1, 1 );
 const material2 = new THREE.MeshBasicMaterial({
 color: 0x00ff00, transparent: true,  
 });
 const Cyb = new THREE.Mesh(geometry2, material2);
+Cyb.position.x = -1.5;
+Cyb.position.y = 1.5;
 
 const geometry3 = new THREE.CircleGeometry( 0.5, 32 ); 
 const material3 = new THREE.MeshBasicMaterial({
 color: 0x0000ff, 
 });
 const Kryg = new THREE.Mesh(geometry3, material3);
+Kryg.position.z = - 1;
+Kryg.position.y = 0;
 
 const anchor = mindarThree.addAnchor(0);
 
